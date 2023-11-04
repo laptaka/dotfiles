955f5fb4e6b426ed860fb1dd516297f31bd5e91c
Lapt <83134094+laptaka@users.noreply.github.com>
2023-11-04 01:44:05 +0100

    PRINTING

--- install.sh
+++ install.sh
@@ -99,6 +99,7 @@ asklapted=${asklapted:-N}
 if [[ $asklapted =~ ^[Yy]$ ]]; then
     echo "sup, me"
     yay -S --noconfirm --needed - < laptbloat.txt
+    sudo systemctl enable --now ecbd.service
     # Replace line hosts: mymachines in /etc/nsswitch.conf with hosts: mymachines mdns_minimal [NOTFOUND=return] resolve [!UNAVAIL=return] files myhostname dns
     sudo sed -i '/hosts: mymachines/c\hosts: mymachines mdns_minimal [NOTFOUND=return] resolve [!UNAVAIL=return] files myhostname dns' /etc/nsswitch.conf
     # Spotify
