c1cd20b081c7ebd875072ae30a8f92d2cf5d0695
Lapt <83134094+laptaka@users.noreply.github.com>
2023-11-05 14:46:55 +0100

    massive whoops

--- Scripts/minimal.sh
+++ Scripts/minimal.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Nvidia
-if nvidia_detect ; then
-    echo "Log: Installing nvidia packages"
-    echo -e "nvidia-dkms\nnvidia-utils" >>packages.txt
-    # replace hyprland with hyprland-nvidia
-    sed -i "s/^hyprland/hyprland-nvidia/g" packages.txt
-else
-    echo "Log: Skipping nvidia packages"
-fi
-
 # Install packages
 echo "Log: Installing packages"
 yay -S --noconfirm --needed - < packages.txt
